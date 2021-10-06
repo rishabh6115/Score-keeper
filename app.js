53f7a419914de49f8d9029f9afb5fc84304e8bf3
Rishabh Ranjan <77900932+rishabh6115@users.noreply.github.com>
2021-10-06 15:45:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+const score2 = document.querySelector('#s')
+const score1 = document.querySelector('#f')
+const dropdown = document.querySelector('select')
+const fbutton = document.querySelector('.one')
+const sbutton = document.querySelector('.two')
+const reset = document.querySelector('.reset')
+const h1 = document.createElement('h1')
+
+let totalpoint = dropdown.value;
+dropdown.addEventListener('change', function(e) {
+    totalpoint = dropdown.value
+})
+let fcount = 0;
+fbutton.addEventListener('click', function(e) {
+    fcount += 1;
+    score1.innerText = `${fcount}`
+    if (fcount === parseInt(dropdown.value)) {
+
+        h1.innerText = `Congratulations player 1 won`
+        document.body.append(h1)
+        score1.style.color = 'green'
+        score2.style.color = 'red'
+        fbutton.disabled = true
+        sbutton.disabled = true
+    }
+})
+let scount = 0;
+sbutton.addEventListener('click', function(e) {
+    scount += 1;
+    score2.innerText = `${scount}`
+    if (scount === parseInt(dropdown.value)) {
+
+        h1.innerText = `Congratulations player 2 won`
+        document.body.append(h1)
+        score1.style.color = 'red'
+        score2.style.color = 'green'
+        fbutton.disabled = true
+        sbutton.disabled = true
+    }
+
+})
+reset.addEventListener('click', function(e) {
+    score1.innerText = '0'
+    fcount = 0;
+    scount = 0;
+    score2.innerText = '0'
+    h1.remove()
+    score1.style.color = 'black'
+    score2.style.color = 'black'
+    fbutton.disabled = false
+    sbutton.disabled = false
+
+})
